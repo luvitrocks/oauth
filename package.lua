54c6727763852b8c8eba6eb42b89cee40d98e4e3
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-07-29 14:27:47 +0300

    update packages

--- package.lua
+++ package.lua
@@ -1,15 +1,15 @@
 -- for older versions of luvit and alternative package managers
 return {
-	name = "luvit-module",
-	version = "0.0.0",
-	description = "",
+	name = "luvit-oauth",
+	version = "0.0.1",
+	description = "OAuth wrapper for luvit.io",
 	repository = {
-		url = "",
+		url = "https://github.com/luvitrocks/luvit-oauth.git",
 	},
 	author = {
-		name = "",
-		email = ""
+		name = "Dmitri Voronianski",
+		email = "dmitri.voronianski@gmail.com"
 	},
-	licenses = {""},
+	licenses = {"MIT"},
 	main = 'init.lua'
 }
