f52fea233ee700f2065356de3532ffcdb30f5cd2
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-08-15 17:58:10 +0300

    version up

--- package.lua
+++ package.lua
@@ -1,7 +1,7 @@
 -- for older versions of luvit and alternative package managers
 return {
 	name = "luvit-oauth",
-	version = "0.1.4",
+	version = "0.1.5",
 	description = "OAuth wrapper for luvit.io",
 	repository = {
 		url = "https://github.com/luvitrocks/luvit-oauth.git",
